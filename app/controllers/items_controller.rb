5d786bd2c0ffaf7f7b7a772f56909ad5de9f4746
springkyu <seika.k.r0610@gmail.com>
2021-08-17 07:14:41 +0000

    [ADD]kaminari設定file

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    @all_items = Item.all
+    
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
