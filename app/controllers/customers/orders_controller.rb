a9ce8148906c5a6bdddc583e5823472c7a76a0c2
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-08-19 11:20:57 +0000

    支払金額まだ、後ほど修正します

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,5 +1,12 @@
 class Customers::OrdersController < ApplicationController
 def index
   @orders = Order.all
+  @total_price = @products.sum(:product_price)
+
+end
+
+def show
+  @order = Order.find(params[:id])
+  @order_items = @oreder.order_items
 end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   belongs_to :customer
+  
+  def add_tax_price
+    (self.price * 1.1).round
+  end
+  
 end
