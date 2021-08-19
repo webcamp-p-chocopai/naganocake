c31e635b927e53c0cf9557a7441cf7dc220cc2c8
KentaMatsu <matsuken102@gmail.com>
2021-08-19 15:24:12 +0000

    ジャンル編集

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,32 +1,31 @@
 class Admin::GenresController < ApplicationController
-  
+
   def index
-    @genre = Genres.new
-    @genres = Genres.all
-    @genre_page = Genres.all.page(params[:page])
+    @genre = Genre.new
+    @genres = Genre.all
   end
-  
+
   def create
     @genre = Genre.new(genre_params)
     @genre.save
     redirect_back(fallback_location: root_path)
   end
-  
+
   def edit
-    @genre = Genres.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
-  
+
   def update
-    @genre = Genres.find(params[:id])
-    @genres = Genres.update(genre_params)
+    @genre = Genre.find(params[:id])
+    @genre = Genre.update(genre_params)
     redirect_to admin_genres_path
   end
-  
+
   private
-  
+
   def genre_params
-    params.require(:genre).permit(:genre_name )
+    params.require(:genre).permit(:genre_name)
   end
-  
-  
+
+
 end
