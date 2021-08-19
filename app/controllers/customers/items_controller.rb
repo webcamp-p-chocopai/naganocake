38328b4b532033626be59f07b0260121ae8da2a4
springkyu <seika.k.r0610@gmail.com>
2021-08-19 03:45:57 +0000

    [編集中]genre変数追加

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -3,10 +3,12 @@ class Customers::ItemsController < ApplicationController
   def index
     @all_items = Item.all
     @items = @all_items.page(params[:page]).reverse_order
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 end
