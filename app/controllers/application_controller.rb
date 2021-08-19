9c28eb2ff85c76ea6580f46ad8f6aae983f8ee4d
springkyu <seika.k.r0610@gmail.com>
2021-08-19 09:07:25 +0000

    [編集]ログインcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
   def after_sign_in_path_for(resource)
-    customers_path(current_customer.id)
+    case resource
+    when Customer
+      customers_path(current_customer.id)
+    when Admin
+      admin_orders
+    end
   end
   
   def after_sign_out_path_for(resource)
-    root_path
+    case resource
+    when Customer
+      root_path
+    when Admin
+      new_admin_session
+    end
   end
   
-  protected
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :kana_last_name, :kana_first_name, :cust_postal_code, :cust_address, :phone_number])
-  end
 end
--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Customers::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -41,9 +41,9 @@ class Customers::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+def configure_sign_up_params
+　devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :kana_last_name, :kana_first_name, :cust_postal_code, :cust_address, :phone_number])
+end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
