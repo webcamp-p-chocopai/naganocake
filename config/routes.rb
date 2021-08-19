cd8d276370c86f9cb573b3745d626dba579e9f93
springkyu <seika.k.r0610@gmail.com>
2021-08-19 05:32:01 +0000

    [ADD]adminルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admin_users/sessions',
+    passwords: 'admin_users/passwords',
+    registrations: 'admin_users/registrations'
+  }
+
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
