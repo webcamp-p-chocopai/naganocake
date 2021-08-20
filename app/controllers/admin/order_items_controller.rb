659aa40774cf796efa6d561ffb7bad8d27da201d
springkyu <seika.k.r0610@gmail.com>
2021-08-20 15:05:28 +0000

    [編集中]admin注文履歴コントローラ

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -5,17 +5,19 @@ class Admin::OrderItemsController < ApplicationController
     order_item = OrderItem.find(params[:id])
     order_item.update(order_item_params)
     case order_item.production_status
+    # 製作ステータスが一つでも"製作中"になったら、注文ステータスが"製作中"に自動更新
       when "製作中"
         order_item.order.update(orderd_status: "製作中")
+    # 製作ステータスが全部"製作完了"になったら、注文ステータスが"発送準備中"に自動更新
       when "製作完了"
-        if order_item.order.order_items.all?
-
-
+        order_item.order.update(orderd_status: "発送準備中")
     end
+    redirect_to admin_order_path(order_item.order.id)
   end
 
   private
   def order_item_params
     params.require(:order_item).permit(:production_status)
   end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -15,6 +15,7 @@ class Admin::OrdersController < ApplicationController
     order = Order.find(params[:id])
     order_items = order.order_items
     order.update(order_params)
+    # 注文ステータスが"入金確認"になったら製作ステータス全てを"製作待ち"に自動更新?
     if order.orderd_status == "入金確認"
       order_items.all_update(production_status: "製作待ち")
     end
