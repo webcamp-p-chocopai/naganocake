c1cfab3d62efe1f83cacba7d9ffef79c7a5ac494
springkyu <seika.k.r0610@gmail.com>
2021-08-20 14:41:17 +0000

    [編集中]admin注文履歴コントローラ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -18,6 +18,11 @@ class Admin::OrdersController < ApplicationController
     if order.orderd_status == "入金確認"
       order_items.all_update(production_status: "製作待ち")
     end
-    redirect_to admin_order_path(orde)
+    redirect_to admin_order_path(order.id)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:orderd_status)
   end
 end
