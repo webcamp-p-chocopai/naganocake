07f9ba4cae016d9d26a842e4fb1666b86765c2c5
springkyu <seika.k.r0610@gmail.com>
2021-08-20 09:51:00 +0000

    [追加]enum定義

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,12 @@
 class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
+
+  enum order_status: {
+    "着手不可" => 0,
+    "製作待ち" => 1,
+    "制作中" => 2,
+    "製作完了" => 3,
+  }
+
 end
