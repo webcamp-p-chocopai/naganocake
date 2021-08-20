20505b765b5345e66114c1dac7d71a12e9b9233b
springkyu <seika.k.r0610@gmail.com>
2021-08-20 06:58:32 +0000

    [編集]admin/ordersコントローラ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,12 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @orders = Order.page(params[:page]).per(10)
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+  end
 end
