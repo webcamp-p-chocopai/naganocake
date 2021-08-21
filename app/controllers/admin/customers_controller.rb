00448fbf131cb7294a3ac7f08ac39fbd8dd45eeb
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-08-21 11:59:34 +0000

    機能○、新規登録のviewのカラム名をいじりました

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,16 +16,12 @@ class Admin::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
-    #is_deletedカラムをtrueに変更することにより削除フラグを立てる
-    # @customer.update(is_deleted: true)
-    # reset_session
-    # flash[:notice] = "退会処理を実行いたしました"
-    # redirect_to admin_customer_path(@customer)
+    redirect_to admin_customer_path(@customer)
   end
   
   protected
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :cust_postal_code, :cust_address, :phone_number, :email)
+    params.require(:customer).permit(:last_name, :first_name, :kana_last_name, :kana_first_name, :cust_postal_code, :cust_address, :phone_number, :email, :is_deleted)
   end
 
 end
