10b87ba40a7694e0a7352fee8d490e0d6aec139f
KentaMatsu <matsuken102@gmail.com>
2021-08-23 08:07:32 +0000

    注文機能追加

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -20,62 +20,75 @@ class Customers::OrdersController < ApplicationController
   end
 
   def create
-    # ↓カート内が空の時にホーム画面に飛ばす(機能するかわからないのでコメントアウト)
+    # ↓カート内が空の時にホーム画面に飛ばす(privateにまとめて記述？)
     #if current_customer.cart_items.blank?
     #  redirect_to root_path
     #end
+    # ↓注文(orders)テーブルに保存
     @order = current_customer.orders.new(order_params)
-    @cart_items = current_cart_items
     @order.save
-    # ↓もし新しいお届け先を入力したときに、配送先テーブルに追加登録する
-    if params[:order][address] == "3"
-      current_customer.addresses.create(address_params)
+    # ↓注文(cart_items)テーブルにデータを入れる(保存する)記述
+    # ↓each文で商品カート(cart_items)をとりだす
+    current_customer.cart_items.each do |cart_item|
+      @order_item = OrderItem.new
+      @order_item.item_id = cart_item.item_id
+      @order_item.purchase_quantity = cart_item.item_qty
+      @order_item.taxed_purchase_price = (cart_item.item.non_taxed_price.add_tax_price).round# ←だめなら*1.1にもどす
+      @order_item.order_id = @order.id
+      @order_item.save
     end
-    # ↓注文(cart_items)テーブルにデータを入れる記述
-    # ↓each文でカラムをとりだして格納？
-    @cart_items.each do
-
-
-
-    #↓注文完了時にカート内のアイテムデータを消す
-    cart_items = current_customer.cart_items
-    cart_items.destroy_all
+    #↓注文完了時にカート内のデータを消してthanxページに飛ぶ
+    current_customer.cart_items.destroy_all
+    redirect_to orders_thanks_path
   end
 
 
   def check
-    # ↓カート内が空の時にホーム画面に飛ばす(機能するかわからないのでコメントアウト)
+    # ↓カート内が空の時にホーム画面に飛ばす(まとめて記述できる？)
     #if current_customer.cart_items.blank?
     #  redirect_to root_path
     #end
     @cart_items = current_customer.cart_items
     @order = Order.new(order_params)
-    @freight = @order.freight + 800
+    @ntax_price = cart_item.item.non_taxed_price
+    @freight = 800
     # ↓商品合計
     @total_price = 0
     @cart_items.each do |cart_item|
-    @total_price += cart_item.item.non_taxed_price * cart_item.item_qty * 1.1
+      @total_price += @ntax_price * cart_item.item_qty.add_tax_price# ←だめなら*1.1にもどす
     end
     # ↓newページのラジオボタンで自身の住所を選択したとき
-    if params[:order][address] == "1"
+    if params[:order][:address_op] == "1"
       @order.postal_code = current_customer.cust_postal_code
       @order.address = current_customer.address
-      @order.dear_name = current_customer.last_name + current_customer.first_name
+      @order.dear_name = current_customer.first_name + current_customer.last_name
       #                  ↑customer_full_name(current_customer)でも可？
     # ↓登録住所から選択したとき
-    elsif params[:order][address] == "2"
-      order_id = current_customer.addresses.find_by
-      @order.postal_code = order_id.
-      @order.address = order_id.
-      @order.dear_name = order_id.
-    # ↓新しいお届け先を選択したとき(不要かも？)
-    else
-      @order.postal_code = params[:order][:postal_code]
-      @order.address = params[:order][:address]
-      @order.dear_name = params[:order][:dear_name]
+    elsif params[:order][:address_op] == "2"
+      # ↓配送先住所のidを特定する
+      @order_id = Address.find(params[:order][:address])
+      # ↓特定したidをもとにそれぞれ呼び出し
+      @order.postal_code = @order_id.postal_code
+      @order.address = @order_id.address
+      @order.dear_name = @order_id.dear_name
+    # ↓新しいお届け先を選択したとき
+    else  # (params[:order][:address_op] == "3"のとき)
+      @address = Address.new()
+      # ↓newページで入力した新しいお届け先のデータを入れる(配送先追加)
+      @address.postal_code = params[:order][:postal_code]
+      @address.address = params[:order][:address]
+      @address.dear_name = params[:order][:dear_name]
+      @address.current_customer_id = current_customer.id
+      if @address.save
+      # ↓注文テーブルにデータを入れる記述(上に記述したデータ流用)
+        @order.postal_code = @address.postal_code
+        @order.address = @address.address
+        @order.dear_name = @address.dear_name
+      else
+        render 'new'
+      end
     end
-　end
-
+  end
 
   def thanks
   end
