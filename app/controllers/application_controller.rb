c18ceac31a02dddfd24c3c99dcd19e7ba7fc5c61
springkyu <seika.k.r0610@gmail.com>
2021-08-24 14:46:47 +0000

    微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Customer
-      customers_path(resource)
+      c_path(resource)
     when Admin
       admin_orders_path
     end
