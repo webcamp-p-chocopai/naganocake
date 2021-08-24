bdcf726463b31e614f87394cefebcaed916107ec
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-08-24 13:27:32 +0000

    バリデーションの追加

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,5 +1,6 @@
 class Customers::AddressesController < ApplicationController
-  
+   before_action :authenticate_customer!
+   
   def index 
     @address = Address.new
     @addresses = Address.all
