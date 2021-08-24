f4c22c40fe192ce7d961657f99b6d980497ddc02
KentaMatsu <matsuken102@gmail.com>
2021-08-24 07:46:22 +0000

    不要ファイル削除

--- app/controllers/customers/homes_controller.rb
+++ app/controllers/customers/homes_controller.rb
@@ -1,5 +1,8 @@
 class Customers::HomesController < ApplicationController
   def top
+    #新着商品を４件のみ表示したい
+    @item = Item.order(created_at: :desc).limit(4)
+    @genres = Genre.all
   end
 
   def about
