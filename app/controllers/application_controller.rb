04e0e5cbc4b4a1d7e81a119dbf1ffda22bf53d26
NakamuraYuka07 <yukaron.8107@gmail.com>
2021-08-24 15:41:18 +0000

    レイアウト作成中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Customer
-      customers_path(resource)
+      root_path(resource)
     when Admin
       admin_orders_path
+    end
+  end
+  
+  def after_sign_up_path_for(resource)
+    case resource
+    when Customer
+      customers_path(resource)
     end
   end
 
