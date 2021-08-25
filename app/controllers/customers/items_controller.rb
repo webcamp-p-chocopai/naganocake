effe57c49ccd99c69312184d56cca60df658c8bf
EC2 Default User <ec2-user@ip-172-31-42-44.ec2.internal>
2021-08-25 05:25:13 +0000

    fix6

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,12 +1,8 @@
 class Customers::ItemsController < ApplicationController
   def index
       @genres = Genre.all
-<<<<<<< HEAD
-      @items = Item.page(params[:page]).per(8)
-=======
       @all_items = Item.where(sale_status: true)
-      @items = @all_items.page(params[:page]).per(8).reverse_order
->>>>>>> 764a2736ec13f93bb813ca1271875a264e6c2852
+      @items = @all_items.page(params[:page]).per(8)
   end
 
   def show
