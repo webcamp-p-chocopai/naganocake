a9074e3fdacc41983ccac38be29c942a90f764c9
springkyu <seika.k.r0610@gmail.com>
2021-08-25 07:06:43 +0000

    [UPDATE]エラー修正+layout微調整

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -4,13 +4,13 @@ class Admin::OrderItemsController < ApplicationController
   def update
     order_item = OrderItem.find(params[:id])
     order_item.update(order_item_params)
-    case order_item.production_status
+    order_items = order_item.order.order_items
     # 製作ステータスが一つでも"製作中"になったら、注文ステータスが"製作中"に自動更新
-      when "製作中"
-        order_item.order.update(orderd_status: "製作中")
+    if order_item.production_status == "製作中"
+      order_item.order.update(orderd_status: "製作中")
     # 製作ステータスが全部"製作完了"になったら、注文ステータスが"発送準備中"に自動更新
-      when "製作完了"
-        order_item.order.update(orderd_status: "発送準備中")
+    elsif order_items.all? {|order_item| order_item.production_status == "製作完了" }
+      order_item.order.update(orderd_status: "発送準備中")
     end
     redirect_back(fallback_location: admin_root_path)
   end
