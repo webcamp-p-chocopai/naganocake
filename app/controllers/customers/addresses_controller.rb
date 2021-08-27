c1fe9d68c66a54616acd8d1266ff22c828daa570
KentaMatsu <matsuken102@gmail.com>
2021-08-27 02:02:44 +0000

    履歴一覧他修正

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,34 +1,34 @@
 class Customers::AddressesController < ApplicationController
    before_action :authenticate_customer!
-   
-  def index 
+
+  def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses.all
   end
-  
+
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     @address.save!
     redirect_to addresses_path
   end
- 
+
   def edit
     @address = Address.find(params[:id])
   end
-  
+
   def update
     @address = Address.find(params[:id])
 		@address.update(address_params)
 		redirect_to addresses_path
   end
-  
+
   def destroy
     @address = Address.find(params[:id])
 	  @address.destroy
     redirect_back(fallback_location: root_path)
-  end 	
-   	
+  end
+
   private
 	def address_params
 		params.require(:address).permit(:postal_code, :address, :dear_name)
